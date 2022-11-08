b9c2e1cb5b41318887782bbed9278b903a949b5c
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-08 16:31:14 +0700

    commit

--- flic.py
+++ flic.py
@@ -4,17 +4,13 @@ FLICKR_PUBLIC = '466c4c31e82b8739e5b07b5798a19129'
 FLICKR_SECRET = 'f854572f81ad7c8a'
 
 flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
-extras='url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
+extras='url_o'
 cats = flickr.photos.search(text='kitten', per_page=5, extras=extras)
 photos = cats['photos']['photo']
 list = []
 for photo in photos:
-    for i in range(0,len(extras),1):
-        try:
-            list.append(photo[extras[i]])
-            break
-        except:
-            pass
+    list.append(photo['url_o'])
+        
 string = '\n'.join(list)
 with open('fick.txt','w',encoding='utf-8') as f:
     f.write(string)
