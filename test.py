d91dd9ce0e528562973bec1b69fd29655dca769c
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-08 16:13:34 +0700

    commit

---
+++ test.py
@@ -0,0 +1,9 @@
+from flickrapi import FlickrAPI
+
+FLICKR_PUBLIC = '466c4c31e82b8739e5b07b5798a19129'
+FLICKR_SECRET = 'f854572f81ad7c8a'
+
+flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
+extras='url_l'
+cats = flickr.photos.search(text='kitten', per_page=100, extras=extras)
+photos = cats['photos']['photo']
