7d3ad11ede6b8073f72cea087617b5d3b6f99801
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-08 15:43:53 +0700

    commit

--- pexels.py
+++ pexels.py
@@ -5,7 +5,7 @@ PEXELS_API_KEY = '563492ad6f91700001000001c6decedb146c4053a03e0b07bee17e8b'
 # Create API object
 api = API(PEXELS_API_KEY)
 # Search five 'kitten' photos
-api.search('kitten', page=1, results_per_page=100)
+api.search('kitten', page=1, results_per_page=1000)
 # Get photo entries
 photos = api.get_entries()
 # Loop the five photos
