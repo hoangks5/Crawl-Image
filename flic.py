3176ff1b3a5b9dd8d4b668bbe38c69d5f49b6175
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-08 16:09:14 +0700

    commit

--- flic.py
+++ flic.py
@@ -5,7 +5,7 @@ FLICKR_SECRET = 'f854572f81ad7c8a'
 
 flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
 extras='url_l'
-cats = flickr.photos.search(text='kitten', per_page=1, extras=extras)
+cats = flickr.photos.search(text='kitten', per_page=2, extras=extras)
 photos = cats['photos']['photo']
 from pprint import pprint
 pprint(photos)
